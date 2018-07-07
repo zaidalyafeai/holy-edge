7cd6b86c541b27c852eec7f58537f546479328f4
Zaid Alyafeai <alyafey22@gmail.com>
2018-07-08 00:34:34 +0300

    Update vgg16.py

--- hed/models/vgg16.py
+++ hed/models/vgg16.py
@@ -18,7 +18,7 @@ class Vgg16():
         self.cfgs = cfgs
         self.io = IO()
 
-        base_path = 'hed/models/vgg16.npy'
+        base_path = 'hed/models/'
         weights_file = os.path.join(base_path, self.cfgs['model_weights_path'])
         
         self.data_dict = np.load(weights_file, encoding='latin1').item()
